d29f1c0157bdecd5ce700ac2ead894650885e624
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2026-04-28 23:30:03 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,20 +1,33 @@
 import sqlite3
 
-conn = sqlite3.connect("ml_pipeline.db")
-cursor = conn.cursor()
+conn=sqlite3.connect("ml_pipeline.db")
+c=conn.cursor()
 
-with open("sql/schema.sql","r") as file:
-    cursor.executescript(
-        file.read()
-    )
+c.execute("""
+CREATE TABLE IF NOT EXISTS input_data(
+id INTEGER PRIMARY KEY,
+feature1 REAL,
+feature2 REAL,
+feature3 REAL,
+feature4 REAL
+)
+""")
 
-sample_data = [
+c.execute("""
+CREATE TABLE IF NOT EXISTS predictions(
+id INTEGER,
+prediction INTEGER,
+prediction_timestamp TEXT
+)
+""")
+
+sample_data=[
 (1,5.1,3.5,1.4,0.2),
 (2,6.2,3.4,5.4,2.3),
 (3,5.9,3.0,4.2,1.5)
 ]
 
-cursor.executemany(
+c.executemany(
 """
 INSERT OR REPLACE INTO input_data
 VALUES (?,?,?,?,?)
@@ -24,4 +37,4 @@ sample_data
 
 conn.commit()
 
-print("Database and sample data created")
+print("Database ready")
