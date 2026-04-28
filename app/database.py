d0c5aaceb32e45acfc3529842afaadda95a8d365
Md Sha Niamatullah <193333628+ns-niam@users.noreply.github.com>
2026-04-28 23:39:41 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -18,23 +18,4 @@ CREATE TABLE IF NOT EXISTS predictions(
 id INTEGER,
 prediction INTEGER,
 prediction_timestamp TEXT
-)
-""")
 
-sample_data=[
-(1,5.1,3.5,1.4,0.2),
-(2,6.2,3.4,5.4,2.3),
-(3,5.9,3.0,4.2,1.5)
-]
-
-c.executemany(
-"""
-INSERT OR REPLACE INTO input_data
-VALUES (?,?,?,?,?)
-""",
-sample_data
-)
-
-conn.commit()
-
-print("Database ready")
